b7f372a7c38f2a7fc4da2775d3a63c47bda39ee2
Cristian Brinza <cristian.brinza.service.acc@gmail.com>
2023-02-23 15:56:11 +0200

    update

---
+++ year2/tm/labs/lab1/script.js
@@ -0,0 +1,319 @@
+var c1_title ='Chapter 1: The Arrival'
+var c1 = ' You wake up in a strange land, surrounded by a dense forest. As you start to explore your surroundings, you notice that you are not alone. A beautiful, mysterious stranger appears out of nowhere and greets you. "Welcome to the Kingdom of Eldoria," she says. "I am Princess Sophia, and you are a guest in our land. What brings you here?"'
+
+    var c1_1= 'Choice 1: - I don`t know how I got here.'
+    var c1_1_continue=' Sophia responds, "That`s okay. We will help you find your way home. In the meantime, please stay with us and enjoy our hospitality."'
+    var c1_1_title ='Chapter 2: The Mystery'
+    var c1_1_text = 'Sophia leads you to the palace, where you are introduced to the King and Queen. They are kind and welcoming, but there is an air of tension in the palace. Sophia tells you that there have been several strange occurrences in the kingdom lately, and they suspect foul play.'
+    
+        var c1_1_1 = 'Choice 1: - I want to help solve the mystery.'
+        var c1_1_1_continue = 'Sophia nods, "We would be grateful for your help. The first step is to investigate the recent incidents. The King and Queen have agreed to let you look around the palace and talk to anyone you think may have information."'
+        var c1_1_1_title = 'Chapter 3: The Suspects'
+        var c1_1_1_text = 'You begin to investigate the recent incidents, and soon you discover that there are several suspects. The cook, the gardener, the stable boy, and the jester all seem to have motives and opportunity. You talk to each of them, but they all seem to be hiding something.'
+    
+            var c1_1_1_1 = 'Choice 1: - I think the cook did it.'
+            var c1_1_1_1_continue = 'You present your evidence to the King and Queen, and they agree that the cook is the most likely suspect. They order him to be arrested, and the mystery is solved.'
+            var c1_1_1_1_title = 'Chapter 4: The Romance'
+            var c1_1_1_1_text = 'As you solve the mystery, you begin to develop a close relationship with Sophia. You spend more and more time together, exploring the kingdom and getting to know each other.'
+    
+            var c1_1_1_1_1 = 'Choice 1: "I want to pursue a relationship with Sophia."'
+            var c1_1_1_1_1_continue ='Sophia blushes, "I have feelings for you too. Let`s see where this goes."'
+            var c1_1_1_1_1_title = 'Chapter 5: The Betrayal'
+            var c1_1_1_1_1_text = 'As your relationship with Sophia grows, you begin to notice that something is not quite right. You discover that Sophia`s advisor, Sir William, is plotting against the King and Queen. He plans to overthrow them and take control of the kingdom.'
+
+                var c1_1_1_1_1_1 ='Choice 1: "I need to tell the King and Queen."'
+                var c1_1_1_1_1_1_continue= 'Sophia agrees, "We must stop Sir William before it`s too late." Together, you and Sophia reveal Sir William`s plan to the King and Queen, and they order his arrest.'
+                var c1_1_1_1_1_1_title ='Chapter 6: The Endgame'
+                var c1_1_1_1_1_1_text = 'The kingdom is safe, and you have solved the mystery and found your way home. You say goodbye to Sophia, notknowing if you will ever see her again.                '
+
+                    var c1_1_1_1_1_1_1 = 'Choice 1: "I want to stay in Eldoria with Sophia."'
+                    var c1_1_1_1_1_1_1_continue='Sophia looks surprised but pleased, "I would love for you to stay with me. Let`s build a life together in Eldoria."'
+                    var c1_1_1_1_1_1_1_title ='Epilogue:'
+                    var c1_1_1_1_1_1_1_text = 'Your choices have shaped the outcome of your adventure in Eldoria. Whether you stayed with Sophia or returned home, the kingdom will always be grateful for your help in solving the mystery and stopping Sir William`s betrayal. As for Sophia, who knows what the future holds? But one thing is for sure, she will always hold a special place in your heart.'
+
+                    
+                    var c1_1_1_1_1_1_2 = 'Choice 2: "I need to go back home."'
+                    var c1_1_1_1_1_1_2_continue= 'Sophia nods, "I understand. Thank you for everything. You will always be welcome in Eldoria."'
+
+                var c1_1_1_1_1_2 ='Choice 2: "I don`t want to get involved."'
+                var c1_1_1_1_1_2_continue ='Sophia looks disappointed but understands, "I`ll take care of it myself. Thank you for your help."                '
+
+            var c1_1_1_1_2 = 'Choice 2: "I`m not interested in romance."'
+            var c1_1_1_1_2_continue ='Sophia looks disappointed but understands, "That`s okay. We can still be friends."'
+
+            var c1_1_1_2 = 'Choice 2: - I`m not sure who did it.'
+            var c1_1_1_2_continue = 'You continue to investigate and gather evidence. Eventually, you discover that the gardener is the real culprit. You present your evidence to the King and Queen, and they order his arrest.'
+        
+        var c1_1_2 = 'Choice 2: - I don`t want to get involved.'
+        var c1_1_2_continue = 'Sophia looks disappointed but understands, "That`s understandable. If you change your mind, let me know."'
+
+    var c1_2= 'Choice 2:  - I am here to find someone.'
+    var c1_2_continue='Sophia asks, "Who are you looking for? Perhaps I can help."'
+    var c1_2_title ='Chapter 1: The Missing Heir'
+    var c1_2_text = 'You tell Sophia that you are searching for the missing heir of a powerful kingdom. The heir disappeared many years ago, and you have been tasked with finding them and bringing them back to their rightful place on the throne. <br> Sophia nods thoughtfully. "I may be able to help you. There have been rumors of a powerful sorcerer who lives in the mountains. He is said to have knowledge of things beyond our understanding, and some even say he can see the future."'
+ 
+        var c1_2_1 ='Choice 1: "I want to go to the mountains to find the sorcerer."'
+        var c1_2_1_continue='Sophia agrees to accompany you on your journey to the mountains. You travel through the dense forest and up the winding paths of the mountains until you finally reach the sorcerer`s lair.'
+        var c1_2_1_title = 'Chapter 2: The Sorcerer`s Lair'
+        var c1_2_1_text = 'You enter the sorcerer`s lair and are immediately struck by the power emanating from within. The sorcerer is an old, wizened man, with eyes that seem to see through your soul.'
+        
+            var c1_2_1_1 = 'Choice 1: "I need your help finding the missing heir."'
+            var c1_2_1_1_continue = 'The sorcerer nods slowly. "I know of whom you speak. The heir is not far from here, but they are in great danger. You must act quickly if you wish to save them."'
+            var c1_2_1_1_title = 'The Conflict:'
+            var c1_2_1_1_text = 'As you search for the missing heir, you begin to realize that the kingdom is not what it seems. The people are afraid, and there are strange rumors of dark magic and evil forces at work. You start to uncover a web of lies and deception that goes far deeper than you ever imagined.'
+
+                var c1_2_1_1_1 = 'Choice 1: "I need to find the heir and get out of here."'
+                var c1_2_1_1_1_continue = 'You and Sophia press on, determined to find the missing heir and escape the kingdom before it`s too late. You encounter many obstacles and dangers along the way, but with your wit and skill, you manage to overcome them all.'
+
+                var c1_2_1_1_1_1 = " to Epilogue:"
+                var c1_2_1_1_1_1_text = " Your journey to Eldoria has come to an end, but the impact of your choices will be felt for years to come. Whether you chose to focus solely on finding the missing heir or to uncover the truth behind the dark magic and evil forces at work, your actions had a significant impact on the fate of the kingdom. As for Sophia, you may have left Eldoria, but the memory of your time together will stay with you always."
+
+                var c1_2_1_1_2 = 'Choice 2: "I need to uncover the truth and stop whoever is behind all of this."'
+                var c1_2_1_1_2_continue = 'You and Sophia decide to stay in the kingdom and uncover the truth behind the dark magic and evil forces at work. You risk everything to confront the mysterious figure behind it all and ultimately succeed in thwarting their plans.'
+
+            var c1_2_1_2 = 'Choice 2: "I don`t need your help. I can find the heir on my own."'
+            var c1_2_1_2_continue =  'The sorcerer looks at you with a mix of amusement and disdain. "Very well. But beware, the kingdom is not what it seems. The heir is in grave danger, and you may find that you need my help after all."'
+
+        var c1_2_2 = 'Choice 2: "I think I can find the heir on my own."'
+        var c1_2_2_continue ='Sophia looks concerned but respects your decision. "Be careful. The kingdom is dangerous, and there are many who would seek to harm you."'
+
+       
+       
+        
+    function randonfunction(min, max) {
+
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+var yourname='';
+haircolor=["black","blonde"]
+var hairrand=randonfunction(0,1);
+yheight=["high","midle","short"]
+var heightrand=randonfunction(0,2);
+
+document.getElementById("audiodiv").innerHTML=
+        '<audio id="myAudio">'+
+        '<source src="audio/start.mp3" type="audio/mpeg">'+
+      '</audio>'
+        document.getElementById("myAudio").play();
+document.getElementById("story").innerHTML=
+'<p align="center">'+
+' <img width=150px; height 150px; src="./image/person.png">'+'</p>'+
+'<p align="center">'+'Cristian Brinza | Lab 1 | TM | FAF-212'+'</p><br>'+
+'<h1 align="center" style="margin-top:-10px;">'+'The Missing person'+'</h1><br>'+
+'<p id="choose" onClick="name()" align="center">'+'Start'+'</p><br>'
+
+function name(){
+    document.getElementById("myAudio").pause();
+    document.getElementById("audiodiv").innerHTML=
+        '<audio id="myAudio">'+
+        '<source src="audio/name.mp3" type="audio/mpeg">'+
+      '</audio>'
+      document.getElementById("myAudio").play();
+   
+    
+    document.getElementById("story").innerHTML=
+    'Input your name: '+
+    '<input type="text" id="fname" name="fname" >'
+    +'<br><br>'+'<p id="choose" onClick="showcaracteristics() " align="center">'+'continue'+'</p><br>'
+    
+            
+}
+
+
+function showcaracteristics(){
+    document.getElementById("myAudio").pause();
+    document.getElementById("audiodiv").innerHTML=
+    '<audio id="myAudio">'+
+    '<source src="audio/char.mp3" type="audio/mpeg">'+
+  '</audio>'
+  document.getElementById("myAudio").play();
+
+    yourname= document.getElementById("fname").value;
+    document.getElementById("story").innerHTML=
+    '<p align="center">'+'<b> Your name: </b>'+yourname+'</p><br>'+
+    '<h2 align="center">'+'<b> Random caracteristics</b></h2><br>'+
+    '<p style=" margin:0" align="center">'+'<b> Your hair color :</b>'+ haircolor[hairrand]+'</p><br>'+
+    '<p align="center">'+'<b> Your height :</b>'+yheight[heightrand]+'</p>'+" "+
+    '<p id="choose" onClick="start()" align="center">'+'Start'+'</p><br>'
+
+
+}
+
+
+function start(){
+    document.getElementById("myAudio").pause();
+    document.getElementById("audiodiv").innerHTML=
+        '<audio id="myAudio">'+
+        '<source src="audio/1.mp3" type="audio/mpeg">'+
+      '</audio>'
+        document.getElementById("myAudio").play();
+document.getElementById("story").innerHTML=
+'<h1 align="center">'+c1_title+'</h1><br>'+
+'<p align="center">'+c1+'</p><br>'+
+'<p id="choose" onClick="f1_1()" align="center">'+c1_1+'</p><br>'+
+'<p id="choose" onClick="f1_2()" align="center">'+c1_2+'</p><br>'
+
+}
+
+function f1_1(){
+    document.getElementById("myAudio").pause();
+    document.getElementById("audiodiv").innerHTML=
+        '<audio id="myAudio">'+
+        '<source src="audio/2.mp3" type="audio/mpeg">'+
+      '</audio>'
+        document.getElementById("myAudio").play();
+    document.getElementById("story").innerHTML=
+    
+    '<p align="center">'+c1_1_continue+'</p><br>'+
+    '<h1 align="center">'+c1_1_title+'</h1><br>'+
+    '<p align="center">'+c1_1_text+'</p><br>'+
+    '<p id="choose" onClick="f1_1_1()" align="center">'+c1_1_1+'</p><br>'+
+    '<p id="choose" onClick="f1_2()" align="center">'+c1_1_2+'</p><br>'+
+    '<p id="choose" onClick="start()" align="center">'+'go back'+'</p><br>'
+}
+
+    function f1_2(){
+        
+        document.getElementById("story").innerHTML=
+        
+        '<p align="center">'+c1_2_continue+'</p><br>'+
+        '<h1 align="center">'+c1_2_title+'</h1><br>'+
+        '<p align="center">'+c1_2_text+'</p><br>'+
+        '<p id="choose" onClick="f1_2_1()" align="center">'+c1_2_1+'</p><br>'+
+        '<p id="choose" onClick="f1_1_1()"  align="center">'+c1_2_2+'</p><br>'+
+        '<p id="choose" onClick="f1_1()" align="center">'+'go back'+'</p><br>'
+    }
+        
+    function f1_2_1(){
+        document.getElementById("story").innerHTML=
+        
+        '<p align="center">'+c1_2_1_continue+'</p><br>'+
+        '<h1 align="center">'+c1_2_1_title+'</h1><br>'+
+        '<p align="center">'+c1_2_1_text+'</p><br>'+
+        '<p id="choose" onClick="f1_2_1_1()" align="center">'+c1_2_1_1+'</p><br>'+
+        '<p id="choose" align="center">'+c1_2_1_2+'</p><br>'
+        +'<p id="choose" onClick="f1_2()" align="center">'+'go back'+'</p><br>'
+    }
+
+    function f1_2_1_1(){
+        document.getElementById("story").innerHTML=
+        
+        '<p align="center">'+c1_2_1_1_continue+'</p><br>'+
+        '<h1 align="center">'+c1_2_1_1_title+'</h1><br>'+
+        '<p align="center">'+c1_2_1_1_text+'</p><br>'+
+        '<p id="choose" onClick="f1_2_ep()" align="center">'+c1_2_1_1_1+'</p><br>'+
+        '<p id="choose" onClick="f1_1_1()" align="center">'+c1_2_1_1_2+'</p><br>'
+        +'<p id="choose" onClick="f1_2_1()" align="center">'+'go back'+'</p><br>'
+    }
+    function f1_2_ep(){
+        document.getElementById("story").innerHTML=
+
+        '<p align="center">'+
+' <img width=150px; height 150px; src="./image/end.png">'+'</p>'+
+        '<p align="center">'+c1_2_1_1_1_1+'</p><br>'+
+        '<p align="center">'+c1_2_1_1_1_1_text+'</p><br><br>'
+        + '<p align="center">'+'End Of Story'+'</p><br>'+
+        '<p id="choose" onClick="name()" align="center">'+'play again'+'</p><br>'
+
+        
+    }
+   
+function f1_1_1(){
+    document.getElementById("myAudio").pause();
+    document.getElementById("audiodiv").innerHTML=
+        '<audio id="myAudio">'+
+        '<source src="audio/3.mp3" type="audio/mpeg">'+
+      '</audio>'
+        document.getElementById("myAudio").play();
+    document.getElementById("story").innerHTML=
+   
+    '<p align="center">'+c1_1_1_continue+'</p><br>'+
+    '<h1 align="center">'+c1_1_1_title+'</h1><br>'+
+    '<p align="center">'+c1_1_1_text+'</p><br>'+
+    '<p id="choose" onClick="f1_1_1_1()" align="center">'+c1_1_1_1+'</p><br>'+
+    '<p id="choose" onClick="f1_2()" align="center">'+c1_1_1_2+'</p><br>'+
+    '<p id="choose" onClick="f1_1()" align="center">'+'go back'+'</p><br>'
+}
+function f1_1_1_1(){
+    document.getElementById("myAudio").pause();
+    document.getElementById("audiodiv").innerHTML=
+        '<audio id="myAudio">'+
+        '<source src="audio/4.mp3" type="audio/mpeg">'+
+      '</audio>'
+        document.getElementById("myAudio").play();
+    document.getElementById("story").innerHTML=
+    '<p align="center">'+
+' <img width=150px; height 150px; src="./image/love1.png">'+'</p>'+
+    '<p align="center">'+c1_1_1_1_continue+'</p><br>'+
+    '<h1 align="center">'+c1_1_1_1_title+'</h1><br>'+
+    '<p align="center">'+c1_1_1_1_text+'</p><br>'+
+    '<p id="choose" onClick="f1_1_1_1_1()" align="center">'+c1_1_1_1_1+'</p><br>'+
+    '<p id="choose" onClick="f1_2()" align="center">'+c1_1_1_1_2+'</p><br>'+
+    '<p id="choose" onClick="f1_1_1()" align="center">'+'go back'+'</p><br>'
+}
+function f1_1_1_1_1(){
+    document.getElementById("myAudio").pause();
+    document.getElementById("audiodiv").innerHTML=
+        '<audio id="myAudio">'+
+        '<source src="audio/5.mp3" type="audio/mpeg">'+
+      '</audio>'
+        document.getElementById("myAudio").play();
+    document.getElementById("story").innerHTML=
+    
+    '<p align="center">'+
+' <img width=150px; height 150px; src="./image/love2.png">'+'</p>'+
+
+    '<p align="center">'+c1_1_1_1_1_continue+'</p><br>'+
+    '<h1 align="center">'+c1_1_1_1_1_title+'</h1><br>'+
+    '<p align="center">'+c1_1_1_1_1_text+'</p><br>'+
+    '<p id="choose" onClick="f1_1_1_1_1_1()" align="center">'+c1_1_1_1_1_1+'</p><br>'+
+    '<p id="choose" onClick="f1_2()" align="center">'+c1_1_1_1_1_2+'</p><br>'+
+    '<p id="choose" onClick="f1_1_1_1()" align="center">'+'go back'+'</p><br>'
+}
+function f1_1_1_1_1_1(){
+
+    document.getElementById("story").innerHTML=
+    
+    '<p align="center">'+
+    ' <img width=150px; height 150px; src="./image/endgame.png">'+'</p>'+
+    document.getElementById("myAudio").pause();
+    document.getElementById("audiodiv").innerHTML=
+        '<audio id="myAudio">'+
+        '<source src="audio/6.mp3" type="audio/mpeg">'+
+      '</audio>'
+        document.getElementById("myAudio").play();
+    '<p align="center">'+c1_1_1_1_1_1_continue+'</p><br>'+
+    '<h1 align="center">'+c1_1_1_1_1_1_title+'</h1><br>'+
+    '<p align="center">'+c1_1_1_1_1_1_text+'</p><br>'+
+    '<p id="choose" onClick="f1_1_1_1_1_1_1()" align="center">'+c1_1_1_1_1_1_1+'</p><br>'+
+    '<p id="choose"onClick="f1_2()"  align="center">'+c1_1_1_1_1_1_2+'</p><br>'+
+    '<p id="choose" onClick="f1_1_1_1_1()" align="center">'+'go back'+'</p><br>'
+}
+function f1_1_1_1_1_1_1(){
+    document.getElementById("story").innerHTML=
+    
+    '<p align="center">'+c1_1_1_1_1_1_1_continue+'</p><br>'+
+    '<p id="choose" onClick="fin()" align="center">'+'to Epilogue'+'</p><br>'+
+    '<p id="choose" onClick="f1_1_1_1_1_1()" align="center">'+'go back'+'</p><br>'
+    
+}
+
+function fin(){
+    document.getElementById("myAudio").pause();
+    document.getElementById("audiodiv").innerHTML=
+        '<audio id="myAudio">'+
+        '<source src="audio/e1.mp3" type="audio/mpeg">'+
+      '</audio>'
+        document.getElementById("myAudio").play();
+    document.getElementById("story").innerHTML=
+    
+    '<p align="center">'+
+' <img width=150px; height 150px; src="./image/end.png">'+'</p>'+
+
+    '<h1 align="center">'+c1_1_1_1_1_1_1_title+'</h1><br>'+
+    '<p align="center">'+c1_1_1_1_1_1_1_text+'</p><br><br>'
+    + '<p align="center">'+'End Of Story'+'</p><br>'+
+    '<p id="choose" onClick="name()" align="center">'+'play again'+'</p><br>'
+}
