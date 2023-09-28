d8717926ad2e11ad1f97e2ff65c339c1cb24389e
Cristian Brinza <cristian.brinza@isa.utm.md>
2023-09-28 11:53:21 +0300

    Update homework.py

--- year3/pr/lab2/homework.py
+++ year3/pr/lab2/homework.py
@@ -3,6 +3,18 @@ import csv
 import requests
 from bs4 import BeautifulSoup
 
+
+# ▒█▀▄▒█▀▄ lab. PR | FAF | FCIM | UTM | Fall 2023
+# ░█▀▒░█▀▄ FAF-212 Cristian Brinza lab2 homework
+
+
+print('')
+print('▒█▀▄▒█▀▄  lab. PR | FAF | FCIM | UTM | Fall 2023')
+print('░█▀▒░█▀▄  FAF-212 Cristian Brinza lab2 homework ')
+print('')
+
+
+
 # User-Agent Rotation: List of user agents to mimic different browsers.
 USER_AGENTS = [
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
@@ -72,6 +84,13 @@ def export_to_csv(filename, data_list):
         dict_writer.writeheader()
         dict_writer.writerows(data_list)
 
+
+def read_urls_from_file(filename):
+    """Reads URLs from a given file."""
+    with open(filename, 'r') as file:
+        return [url.strip() for url in file.readlines()]
+
+
 def main():
     """Main function to interact with the user and perform operations."""
     all_data = []
@@ -79,8 +98,9 @@ def main():
     while True:
         print("\n--- Menu ---")
         print("1. Scrape product details")
-        print("2. Export all scraped data")
-        print("3. Exit")
+        print("2. Scrape product details from a .txt file")
+        print("3. Export all scraped data")
+        print("4. Exit")
         choice = input("Enter your choice: ")
 
         if choice == "1":
@@ -101,25 +121,58 @@ def main():
                     print(f"Scraped data for {url}")
 
         elif choice == "2":
+            # Scrape URLs from a .txt file
+            file_path = input("Enter the path to the .txt file containing the product URLs (without extension): ")+".txt"
+            try:
+                target_urls = read_urls_from_file(file_path)
+                for url in target_urls:
+                    parsed_content = fetch_web_content(url)
+                    if parsed_content:
+                        owner_details = extract_owner_details(parsed_content)
+                        product_info = extract_product_info(parsed_content)
+
+                        final_data = {
+                            'URL': url,
+                            'Owner Details': owner_details,
+                            'Product Info': product_info
+                        }
+                        all_data.append(final_data)
+                        print(f"Scraped data for {url}")
+            except FileNotFoundError:
+                print(f"Error: File '{file_path}' not found!")
+
+        elif choice == "3":
             print("\n--- Export Options ---")
             print("1. JSON")
-            print("2. CSV")
+            print("2. TXT")
+            print("3. CSV")
+            print('4. Console')
             export_choice = input("Enter your choice: ")
 
-            filename = input("Enter filename (without extension): ")
+
 
             if export_choice == "1":
+                filename = input("Enter filename (without extension): ")
                 with open(f"{filename}.json", "w") as outfile:
                     outfile.write(json.dumps(all_data, indent=2, ensure_ascii=False))
                 print(f"Data saved to {filename}.json")
             elif export_choice == "2":
+                filename = input("Enter filename (without extension): ")
+                with open(f"{filename}.txt", "w") as outfile:
+                    outfile.write(json.dumps(all_data, indent=2, ensure_ascii=False))
+                print(f"Data saved to {filename}.txt")
+            elif export_choice == "3":
+                filename = input("Enter filename (without extension): ")
                 # Export to CSV: Save the scraped data into a CSV file.
                 export_to_csv(f"{filename}.csv", all_data)
                 print(f"Data saved to {filename}.csv")
+            elif export_choice == "4":
+                print(json.dumps(final_data, indent=2, ensure_ascii=False))  # Print the scraped data to console
+
             else:
                 print("Invalid choice!")
 
-        elif choice == "3":
+        elif choice == "4":
             print("Exiting program. Goodbye!")
             break
         else:
