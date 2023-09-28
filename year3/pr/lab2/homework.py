c882e16ab725a48074fb8870b97ef2747f587de3
Cristian Brinza <cristian.brinza@isa.utm.md>
2023-09-28 10:38:04 +0300

    Update homework.py

--- year3/pr/lab2/homework.py
+++ year3/pr/lab2/homework.py
@@ -1,11 +1,30 @@
 import json
+import csv
 import requests
 from bs4 import BeautifulSoup
 
+# User-Agent Rotation: List of user agents to mimic different browsers.
+USER_AGENTS = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.3',
+    # Add more user agents if needed
+]
+
 def fetch_web_content(product_url):
     """Fetches the content of the given URL."""
-    response = requests.get(product_url)
-    return BeautifulSoup(response.content, 'html.parser')
+    # User-Agent Rotation: Select a user agent based on the hash of the URL.
+    headers = {
+        'User-Agent': USER_AGENTS[hash(product_url) % len(USER_AGENTS)]
+    }
+    try:
+        # Error Handling: Catch network errors or invalid URLs.
+        response = requests.get(product_url, headers=headers)
+        response.raise_for_status()
+        return BeautifulSoup(response.content, 'html.parser')
+    except requests.RequestException as e:
+        print(f"Error fetching URL {product_url}. Error: {e}")
+        return None
 
 
 def extract_owner_details(parsed_content):
@@ -44,49 +63,67 @@ def extract_product_info(parsed_content):
     return product_info
 
 
+def export_to_csv(filename, data_list):
+    """Exports the scraped data to a CSV file."""
+    # Export to CSV: Write the scraped data into a CSV format.
+    keys = data_list[0].keys()
+    with open(filename, 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(data_list)
+
 def main():
     """Main function to interact with the user and perform operations."""
+    all_data = []
+
     while True:
         print("\n--- Menu ---")
         print("1. Scrape product details")
-        print("2. Exit")
+        print("2. Export all scraped data")
+        print("3. Exit")
         choice = input("Enter your choice: ")
 
         if choice == "1":
-            target_url = input("Enter the product URL: ")
-            parsed_content = fetch_web_content(target_url)
-            owner_details = extract_owner_details(parsed_content)
-            product_info = extract_product_info(parsed_content)
+            # Scrape Multiple URLs: Allow user to input multiple URLs separated by commas.
+            target_urls = input("Enter the product URLs (comma-separated): ").split(',')
+            for url in target_urls:
+                parsed_content = fetch_web_content(url.strip())
+                if parsed_content:
+                    owner_details = extract_owner_details(parsed_content)
+                    product_info = extract_product_info(parsed_content)
 
-            final_data = {
-                'Owner Details': owner_details,
-                'Product Info': product_info
-            }
+                    final_data = {
+                        'URL': url,
+                        'Owner Details': owner_details,
+                        'Product Info': product_info
+                    }
+                    all_data.append(final_data)
+                    print(f"Scraped data for {url}")
 
-            print("\n--- Output Options ---")
-            print("1. Display on console")
-            print("2. Write to file")
-            output_choice = input("Enter your choice: ")
+        elif choice == "2":
+            print("\n--- Export Options ---")
+            print("1. JSON")
+            print("2. CSV")
+            export_choice = input("Enter your choice: ")
 
-            if output_choice == "1":
-                print("\nScraped Data:")
-                print(final_data)
-            elif output_choice == "2":
-                filename = input("Enter filename (default: storage.txt): ")
-                if not filename:
-                    filename = "storage.txt"
-                with open(filename, "w") as outfile:
-                    outfile.write(json.dumps(final_data, indent=2, ensure_ascii=False))
-                print(f"Data saved to {filename}")
+            filename = input("Enter filename (without extension): ")
+
+            if export_choice == "1":
+                with open(f"{filename}.json", "w") as outfile:
+                    outfile.write(json.dumps(all_data, indent=2, ensure_ascii=False))
+                print(f"Data saved to {filename}.json")
+            elif export_choice == "2":
+                # Export to CSV: Save the scraped data into a CSV file.
+                export_to_csv(f"{filename}.csv", all_data)
+                print(f"Data saved to {filename}.csv")
             else:
                 print("Invalid choice!")
 
-        elif choice == "2":
+        elif choice == "3":
             print("Exiting program. Goodbye!")
             break
         else:
             print("Invalid choice! Please try again.")
-
 
 if __name__ == '__main__':
     main()
